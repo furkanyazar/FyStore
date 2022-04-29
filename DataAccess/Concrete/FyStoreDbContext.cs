f8ad64df497c94b16694eadf1c5704265e31f765
Furkan Yazar <furkanyazar9853@gmail.com>
2022-04-29 20:30:41 +0300

    Add project files.

---
+++ DataAccess/Concrete/FyStoreDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Core.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Concrete
+{
+    public class FyStoreDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=localhost;Database=FyStore;Trusted_Connection=true");
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<OperationClaim> OperationClaims { get; set; }
+        public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
+    }
+}
